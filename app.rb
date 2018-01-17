c74db58a359e2cd04f6dd66cb7453c6d60c9f0d3
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-17 09:27:21 +0000

    refactored to have redirect

--- app.rb
+++ app.rb
@@ -1,18 +1,25 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     erb(:index)
   end
-
   # start the server if ruby file executed directly
   run! if app_file == $0
 
-
   post '/names' do
     p params
-    @name_1 = params[:name_1]
-    @name_2 = params[:name_2]
-    erb(:names)
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
+    erb(:play)
   end
 end
--- spec/features/testing_name_fill_spec.rb
+++ spec/features/testing_name_fill_spec.rb
@@ -4,6 +4,6 @@ feature 'Testing name fill' do
     fill_in "name_1", :with => "Jennifer"
     fill_in "name_2", :with => "Chris"
       click_button "Submit"
-    expect(page).to have_text("Jennifer vs Chris")
+    expect(page).to have_text("Jennifer vs. Chris")
   end
 end
