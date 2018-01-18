bba4be7d8998b08c5e256256adf8dddc569a8450
ealitten <ea.litten@gmail.com>
2018-01-18 12:03:04 +0000

    Re-enable feature tests

--- spec/features/testing_name_fill_spec.rb
+++ spec/features/testing_name_fill_spec.rb
@@ -1,6 +1,6 @@
-# feature 'Testing name fill' do
-#   scenario 'Can receive names' do
-#     sign_in_and_play
-#     expect(page).to have_text("Jennifer vs. Chris")
-#   end
-# end
+feature 'Testing name fill' do
+  scenario 'Can receive names' do
+    sign_in_and_play
+    expect(page).to have_text("Jennifer vs. Chris")
+  end
+end
